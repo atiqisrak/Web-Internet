175720430e6e428809468988449befea8a676455
Niloy Niil <22797857+niloyniil@users.noreply.github.com>
2019-11-24 08:23:35 +0600

    Add files via upload
    Assignment Javascript

---
+++ prob1.js
@@ -0,0 +1,9 @@
+function result() {
+	var i1 = document.getElementById("int1".innerHTML)
+	var i2 = document.getElementById("int2".innerHTML)
+	var i3 = document.getElementById("int3".innerHTML)
+
+	var result = i1+i2+i3;
+
+	document.write(result);
+}
